deb3a7228141c22e0915302a13fc1b42f603d9c5
RoyDMAP <roy.dimapilis@yahoo.com>
2025-10-06 17:33:56 -0700

    Initial Commit

---
+++ AcceloCubePro/AcceloCubeProApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcceloCubeProApp.swift
+//  AcceloCubePro
+//
+//  Created by Roy Dimapilis on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AcceloCubeProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
